01396f3a987d81932fa42ff7e8be280cf2e991e0
maxpoy <Menace54>
2014-09-24 15:00:28 +0200

    lol

--- src/ArbreBPLus.java
+++ src/ArbreBPLus.java
@@ -1,4 +1,19 @@
 
 public class ArbreBPLus {
 	
+	private int ordre;
+	private int TauxDeRemplissage;
+	
+	
+	public ArbreBPLus(int n){
+		ordre = n;
+	}
+	
+	public void Insertion(){
+		
+	}
+	
+	public static void main(String[] args){
+		
+	}
 }
